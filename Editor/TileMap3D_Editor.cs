bc1ac1f481769acf6a8634c093058fa3070d62f9
David Díaz <jdaviddiaz64@gmail.com>
2021-08-09 00:12:17 -0500

    Update TileMap3D_Editor.cs
    *Added fill of underground.
    *Added topLeft rule.

--- Editor/TileMap3D_Editor.cs
+++ Editor/TileMap3D_Editor.cs
@@ -8,6 +8,7 @@ public class TileMap3D_Editor : Editor
 {
     Dictionary<Vector2Int, int> yValues = new Dictionary<Vector2Int, int>();
     Dictionary<Vector2Int, TileType> tileTypesAtPositions = new Dictionary<Vector2Int, TileType>();
+    bool[,,] isFilled;
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
@@ -21,9 +22,26 @@ public class TileMap3D_Editor : Editor
     {
         CalculateYValues(tilemap);
 
+        isFilled = new bool[tilemap.heightMap.width, tilemap.numberOfFloors, tilemap.heightMap.height];
+        for (int i = 0; i < tilemap.heightMap.width; i++)
+        {
+            for (int j = 0; j < tilemap.numberOfFloors; j++)
+            {
+                for (int k = 0; k < tilemap.heightMap.height; k++)
+                {
+                    if (j <= yValues[new Vector2Int(i, k)])
+                        isFilled[i, j, k] = true;
+                    else
+                        isFilled[i, j, k] = false;
+                }
+            }
+        }
+
         tileTypesAtPositions.Clear();
 
         CheckRule(TileType.Fill, tilemap);
+
+        CheckRule(TileType.CornerUpLeft, tilemap);
 
         InstatiateTileObjects(tilemap);
 
@@ -31,6 +49,8 @@ public class TileMap3D_Editor : Editor
 
     void InstatiateTileObjects(TileMap3D tilemap)
     {
+        GameObject parent = new GameObject("map");
+        parent.transform.parent = tilemap.transform;
         for (int i = 0; i < tilemap.heightMap.width; i++)
         {
             for (int j = 0; j < tilemap.heightMap.height; j++)
@@ -38,12 +58,22 @@ public class TileMap3D_Editor : Editor
                 Vector2Int gridCord = new Vector2Int(i, j);
                 TileType t = tileTypesAtPositions[gridCord];
                 Vector3 position = new Vector3(i * tilemap.tileX, yValues[gridCord] * tilemap.tileHeight, j * tilemap.tileZ);
+                GameObject ob = null;
                 switch (t)
                 {
                     case TileType.Fill:
-                        GameObject ob = GameObject.Instantiate(tilemap.tileFill, position, Quaternion.identity);
-                        ob.transform.parent = tilemap.transform;
+                        ob = GameObject.Instantiate(tilemap.tileFill, position, Quaternion.identity);
                         break;
+                    case TileType.CornerUpLeft:
+                        ob = GameObject.Instantiate(tilemap.tileCorner, position, Quaternion.identity);
+                        break;
+
+                }
+                ob.transform.parent = parent.transform;
+                for (int k=0; k<yValues[gridCord]; k++)
+                {
+                    ob = GameObject.Instantiate(tilemap.tileFill, new Vector3(position.x, k*tilemap.tileHeight, position.z), Quaternion.identity);
+                    ob.transform.parent = parent.transform;
                 }
                
             }
@@ -52,18 +82,34 @@ public class TileMap3D_Editor : Editor
 
     void CheckRule( TileType t, TileMap3D tilemap)
     {
-        switch (t)
+        for(int i=0; i< tilemap.heightMap.width; i++)
         {
-            case TileType.Fill:
-                for (int i = 0; i < tilemap.heightMap.width; i++)
+            for (int j = 0; j < tilemap.heightMap.height; j++)
+            {
+                Vector2Int cord = new Vector2Int(i, j);
+                switch (t)
                 {
-                    for(int j=0; j< tilemap.heightMap.height; j++)
-                    {
-                        tileTypesAtPositions[new Vector2Int(i, j)] = TileType.Fill;
-                    }
+                    case TileType.Fill:
+                        tileTypesAtPositions[cord] = TileType.Fill;
+                        break;
+                    case TileType.CornerUpLeft:
+                        bool flag = true;
+                        int yvalue = yValues[cord];
+                        if (i - 1 >= 0 && isFilled[i - 1, yValues[cord],j])
+                            flag = false;
+                        if (j + 1 < tilemap.heightMap.height && isFilled[i , yValues[cord], j + 1])
+                            flag = false;
+                        if (i + 1 < tilemap.heightMap.width && !isFilled[i + 1 , yValues[cord], j])
+                            flag = false;
+                        if (j - 1 >= 0  && !isFilled[i , yValues[cord], j - 1])
+                            flag = false;
+                        if (flag)
+                            tileTypesAtPositions[cord] = TileType.CornerUpLeft;
+                        break;
                 }
-                break;
+            }
         }
+        
 
     }
 
