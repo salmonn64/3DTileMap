03438c08bd21d08d351e90d7514e05e284a3fd28
David Díaz <jdaviddiaz64@gmail.com>
2021-08-09 02:27:18 -0500

    Update TileMap3D_Editor.cs
    * Rule for tiles code has been rewrote so it's not repetitive and dumb.

--- Editor/TileMap3D_Editor.cs
+++ Editor/TileMap3D_Editor.cs
@@ -21,7 +21,6 @@ public class TileMap3D_Editor : Editor
     void CreateMap(TileMap3D tilemap)
     {
         CalculateYValues(tilemap);
-
         isFilled = new bool[tilemap.heightMap.width, tilemap.numberOfFloors, tilemap.heightMap.height];
         for (int i = 0; i < tilemap.heightMap.width; i++)
         {
@@ -39,10 +38,11 @@ public class TileMap3D_Editor : Editor
 
         tileTypesAtPositions.Clear();
 
-        CheckRule(TileType.Fill, tilemap);
-
-        CheckRule(TileType.CornerUpLeft, tilemap);
-
+        for(int i=1; i<=9; i++)
+        {
+            CheckRule((TileType)i, tilemap);
+        }
+        
         InstatiateTileObjects(tilemap);
 
     }
@@ -66,6 +66,18 @@ public class TileMap3D_Editor : Editor
                         break;
                     case TileType.CornerUpLeft:
                         ob = GameObject.Instantiate(tilemap.tileCorner, position, Quaternion.identity);
+                        break;
+                    case TileType.CornerUpRight:
+                        ob = GameObject.Instantiate(tilemap.tileCorner, position, Quaternion.identity);
+                        ob.transform.localScale = new Vector3(-ob.transform.localScale.x, ob.transform.localScale.y, ob.transform.localScale.z);
+                        break;
+                    case TileType.CornerDownRight:
+                        ob = GameObject.Instantiate(tilemap.tileCorner, position, Quaternion.identity);
+                        ob.transform.localScale = new Vector3(ob.transform.localScale.x, ob.transform.localScale.y, -ob.transform.localScale.z);
+                        break;
+                    case TileType.CornerDownLeft:
+                        ob = GameObject.Instantiate(tilemap.tileCorner, position, Quaternion.identity);
+                        ob.transform.localScale = new Vector3(-ob.transform.localScale.x, ob.transform.localScale.y, -ob.transform.localScale.z);
                         break;
 
                 }
@@ -93,24 +105,59 @@ public class TileMap3D_Editor : Editor
                         tileTypesAtPositions[cord] = TileType.Fill;
                         break;
                     case TileType.CornerUpLeft:
-                        bool flag = true;
-                        int yvalue = yValues[cord];
-                        if (i - 1 >= 0 && isFilled[i - 1, yValues[cord],j])
-                            flag = false;
-                        if (j + 1 < tilemap.heightMap.height && isFilled[i , yValues[cord], j + 1])
-                            flag = false;
-                        if (i + 1 < tilemap.heightMap.width && !isFilled[i + 1 , yValues[cord], j])
-                            flag = false;
-                        if (j - 1 >= 0  && !isFilled[i , yValues[cord], j - 1])
-                            flag = false;
-                        if (flag)
+                        if (CheckRuleForCell(cord, new int[] { 0, -1, 0, -1, 1, 0, 1, 0 }, tilemap ))
                             tileTypesAtPositions[cord] = TileType.CornerUpLeft;
                         break;
+                    case TileType.CornerUpRight:
+                        if (CheckRuleForCell(cord, new int[] { 0, -1, 0, 1, -1, 0, 1, 0 }, tilemap))
+                            tileTypesAtPositions[cord] = TileType.CornerUpRight;
+                        break;
+                    case TileType.CornerDownRight:
+                        if (CheckRuleForCell(cord, new int[] { 0, 1, 0, -1, 1, 0, -1, 0 }, tilemap))
+                            tileTypesAtPositions[cord] = TileType.CornerDownRight;
+                        break;
+                    case TileType.CornerDownLeft:
+                        if( CheckRuleForCell(cord, new int[]{ 0, 1, 0, 1, -1, 0, -1, 0 }, tilemap))
+                        tileTypesAtPositions[cord] = TileType.CornerDownLeft;
+                        break;
+
                 }
             }
         }
-        
+    }
 
+    bool CheckRuleForCell(Vector2Int cell, int[] rule, TileMap3D tilemap)
+    {
+        //0|1|2
+        //3|X|4
+        //6|7|8
+        Dictionary<int, Vector2Int> adyacentCells = new Dictionary<int, Vector2Int>();
+        adyacentCells.Add(0, cell + new Vector2Int(-1, 1));
+        adyacentCells.Add(1, cell + new Vector2Int(0, 1));
+        adyacentCells.Add(2, cell + new Vector2Int(1, 1));
+        adyacentCells.Add(3, cell + new Vector2Int(-1, 0));
+        adyacentCells.Add(4, cell + new Vector2Int(1, 0));
+        adyacentCells.Add(5, cell + new Vector2Int(-1, -1));
+        adyacentCells.Add(6, cell + new Vector2Int(0, -1));
+        adyacentCells.Add(7, cell + new Vector2Int(1, -1));
+        for(int i=0; i< 8; i++)
+        {
+            if(rule[i] != 0 && CordsInRange(adyacentCells[i], tilemap) )
+            {
+                if (rule[i] == 1 && !isFilled[adyacentCells[i].x, yValues[cell], adyacentCells[i].y])
+                    return false;
+                if (rule[i] == -1 && isFilled[adyacentCells[i].x, yValues[cell], adyacentCells[i].y])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    bool CordsInRange(Vector2Int v, TileMap3D tilemap)
+    {
+        if (v.x >= 0 && v.x < tilemap.heightMap.width && v.y < tilemap.heightMap.height && v.y >= 0)
+            return true;
+        else return false;
     }
 
     void CalculateYValues(TileMap3D tilemap)
@@ -136,11 +183,11 @@ public class TileMap3D_Editor : Editor
 
     enum TileType
     {
-        CornerUpLeft = 1,
+        Fill = 1,
         SideUp = 2,
         CornerUpRight = 3,
         SideLeft = 4,
-        Fill = 5,
+        CornerUpLeft = 5,
         SideRight = 6,
         CornerDownLeft = 7,
         SideDown = 8,
